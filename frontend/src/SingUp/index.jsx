a4e64b7b5a766ae807b4cff16d970f730b4f81c7
Salih korkmaz <salihkorkmaz108478@gmail.com>
2024-07-29 09:13:41 +0300

    v-2

--- frontend/src/SingUp/index.jsx
+++ frontend/src/SingUp/index.jsx
@@ -1,30 +1,32 @@
+import React, { useState } from 'react';
+
 export function SignUp() {
 
     const [username, setUsername] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [passwordRepeat, setPasswordRepeat] = useState()
 
-    const onChangeUsername = (event) => {
-        setUsername(event.target.value);
-    }
-    console.log(username)
+
     return (
         <>
             <h1>Sign Up</h1>
             <div>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" placeholder="Username" onChange={onChangeUsername}/>
+                <input id="username" type="text" placeholder="Username" onChange={(event) => setUsername(event.target.value)}/>
             </div>
             <div>
                 <label htmlFor="email">E-mail</label>
-                <input id="email" type="email" placeholder="E-mail"/>
+                <input id="email" type="email" placeholder="E-mail" onChange={(event) => setEmail(event.target.value)}/>
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" placeholder="Password"/>
+                <input id="password" type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
             </div>
             <div>
                 <label htmlFor="passwordRepeat">Password Repeat</label>
-                <input id="passwordRepeat" type="password" placeholder="Password"/>
+                <input id="passwordRepeat" type="password" placeholder="Password" onChange={(event) => setPasswordRepeat(event.target.value)}/>
             </div>
-            <button type="submit">Sign Up</button>
+            <button disabled={!password || password !== passwordRepeat }>Sign Up</button>
         </>)
 }
